d58a1a48b916571db5a7af1343ff27852c448f4e
georgt99 <33638816+georgt99@users.noreply.github.com>
2020-09-26 16:42:38 +0200

    leg movement, colliders still not functional

--- scenes/schaf/leg.gd
+++ scenes/schaf/leg.gd
@@ -3,16 +3,19 @@ extends Node2D
 #export var torque := 10000.0
 export var force := 100.0
 export var input : String = "ui_up"
+var move_distance = 50
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed(input):
 		var parent : RigidBody2D = get_parent().get_parent()
 		#parent.add_torque(torque)
 		parent.apply_impulse(position, Vector2.UP.rotated(get_rot()) * force)
+		
+		position += position.normalized() * move_distance
 
 	if Input.is_action_just_released(input):
-		pass
+		position -= position.normalized() * move_distance
 
 
 func get_rot():
-	return fmod(get_parent().get_parent().rotation + rotation, 2*PI)
+	return fmod(get_parent().get_parent().rotation + get_parent().rotation, 2*PI)
