852e4507f13945c79534ccdee0914353abf91157
Paul Methfessel <paul.methfessel@web.de>
2020-09-26 19:40:58 +0200

    Fix parallax

--- scenes/back/parallax.gd
+++ scenes/back/parallax.gd
@@ -4,6 +4,6 @@ export var factor: float = 0.5
 export var static_pos: float = 0
 
 func _process(delta):
-	var cam = get_parent().get_node("Schaf") # historisch gewachsen
-	var cam_pos = cam.position.x
+	var cam = get_parent().get_node("Schaf/Camera2D")
+	var cam_pos = cam.get_camera_screen_center().x
 	position.x = cam_pos * 0.5 + static_pos
