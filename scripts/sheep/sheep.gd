e5ea363715d37df3e99441ec46bb34c1c421d56b
georgt99 <33638816+georgt99@users.noreply.github.com>
2020-09-27 14:24:32 +0200

    fart indicator

--- scripts/sheep/sheep.gd
+++ scripts/sheep/sheep.gd
@@ -19,7 +19,32 @@ export var charge_time = 2
 export var max_multiplier = 2 - 1
 export var cooldown = 3
 var _last_usage = -cooldown
-var _charge_start
+var _charge_start = -1 # -1 = not charging
+
+var _indicator : Node2D
+
+export var indicator_image : Texture
+export var indicator_color : Color
+
+func _ready():
+	_indicator = preload("res://scenes/menus/Indicator.tscn").instance()
+	self.add_child(_indicator)
+	_indicator.global_position = target_indicator_position()
+	_indicator.scale = Vector2(0.7, 0.7)
+	_indicator.set_image(indicator_image)
+	_indicator.set_color(indicator_color)
+
+
+func _process(delta):
+	_indicator.move_towards(target_indicator_position())
+	
+	if _charge_start == -1:
+		var percentage = min((OS.get_ticks_msec()/1000.0 - _last_usage) / cooldown, 1.0)
+		_indicator.ready_progress(percentage)
+	else:
+		var percentage = min(((OS.get_ticks_msec()/1000.0 - _charge_start) / charge_time), 1)
+		_indicator.charge_progress(percentage)
+
 
 func _physics_process(delta):
 	if Input.is_action_just_pressed("fart") && _last_usage + cooldown < OS.get_ticks_msec()/1000:
@@ -29,12 +54,16 @@ func _physics_process(delta):
 		_last_usage = OS.get_ticks_msec()/1000
 		var multiplier = (min(((OS.get_ticks_msec()/1000.0 - _charge_start) / charge_time), 1)) * max_multiplier + 1
 		_charge_start = -1
+		_indicator.charge_progress(0)
 		
 		var direction = Vector2.RIGHT.rotated(rotation - PI/8).normalized() # bottom left
 		var particles : Particles2D = preload("res://scenes/sheep/fart_particles.tscn").instance()
 		particles.amount *= multiplier
-		particles.position = - direction * $MainCol.shape.radius
+		particles.position = - Vector2.RIGHT.rotated(- PI/8).normalized() * $MainCol.shape.radius
 		self.add_child(particles)
 		#particles.rotation = rotation #+ 3/8 * PI
 		
 		self.apply_central_impulse(direction * base_boost * multiplier)
+
+func target_indicator_position():
+	return global_position + Vector2.LEFT.rotated(rotation - PI/8).normalized() * ($MainCol.shape.radius + 100) # bottom left
